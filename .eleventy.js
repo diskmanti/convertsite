cd392c07d31df9d52c2de0ab351834035bec7f27
Amanti Lulo <amanti.lulo@gov.local>
2019-07-15 11:27:52 +0200

    finished welcome page and varous layouts

--- .eleventy.js
+++ .eleventy.js
@@ -33,6 +33,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("favicon.ico")
 
   /* Markdown Plugins */
   let markdownIt = require("markdown-it");
---
+++ about/index.md
@@ -0,0 +1,9 @@
+---
+layout: layouts/welcome
+title: About me
+tags:
+  - nav
+navtitle: About
+---
+
+this is my about page
